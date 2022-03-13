d7e89e3bed1a385c19aad9e98f874eea761c2471
Blake Hakkila <blake.austin.hakkila@gmail.com>
2022-03-12 19:10:13 -0800

    updated modbus test for number of registers fix

--- software/firmware/ModbusDevelopment/modbus_test.py
+++ software/firmware/ModbusDevelopment/modbus_test.py
@@ -37,20 +37,20 @@ def modRTU_CRC(packet,len):
                 crc ^= 0xA001
             else:
                 crc >>= 1
-    crcar = crc.to_bytes(2,'big')
+    crcar = crc.to_bytes(2, 'little')
     return crcar
 
-node = serial.Serial('COM5',19200, timeout=0.01)
+node = serial.Serial('COM5',115200, timeout=0.01)
 
 """
 write command (from master)
-[slave id, function code, start register bytes (x2), end register bytes (x2), number of bytes, data bytes (x???), CRC bytes(x2) ]
+[slave id, function code, start register bytes (x2), number of registers bytes (x2), number of bytes, data bytes (x???), CRC bytes(x2)]
 
 write response (from slave)
-[slave id, function code, start register bytes(x2), end register bytes (x2), CRC bytes (x2)]
+[slave id, function code, start register bytes(x2), number of registers bytes (x2), CRC bytes (x2)]
 
 read command (from master)
-[slave id, function code, start register bytes(x2), end register bytes (x2), CRC bytes (x2)]
+[slave id, function code, start register bytes(x2), number of registers bytes (x2), CRC bytes (x2)]
 
 read response (from slave)
 [slave id, function code, number of bytes, data bytes (x???), CRC bytes(x2)]
@@ -58,11 +58,11 @@ read response (from slave)
 
 test_packets = [
     [0x01, 0x10, 0x00, 0x00, 0x00, 0x01, 0x02, 0x42, 0x42, 0x16, 0xc1],#, #write 16962 to int reg
-    [0x01, 0x10, 0x01, 0x00, 0x01, 0x01, 0x04, 0x40, 0x48, 0xf5, 0xc3, 0x7c, 0x1b], #write 3.14 to float reg
-    [0x01, 0x10, 0x02, 0x00, 0x02, 0x01, 0x01, 0x42, 0x40, 0x3d], #write 'B' to char reg
-    [0x01, 0x10, 0x03, 0x00, 0x03, 0x01, 0x01, 0x01, 0x01, 0xe1], #write True to bool reg
+    [0x01, 0x10, 0x01, 0x00, 0x00, 0x01, 0x04, 0x40, 0x48, 0xf5, 0xc3, 0x6c, 0xdb], #write 3.14 to float reg
+    [0x01, 0x10, 0x02, 0x00, 0x00, 0x01, 0x01, 0x42, 0x41, 0x85], #write 'B' to char reg
+    [0x01, 0x10, 0x03, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0xa5], #write True to bool reg
     [0x69, 0x02],        #little bit of garbage data  This gets replaced with huge random packet
-    [0x01, 0x03, 0x01, 0x00, 0x01, 0x00, 0x45, 0xa6],               #read first float register (should be pi)
+    [0x01, 0x03, 0x01, 0x00, 0x00, 0x01, 0x85, 0xf6],               #read first float register (should be pi)
     [0x00, 0x01, 0x02, 0x03, 0x90, 0x91, 0x92, 0x93, 0x94, 0x69],   #more garbage data
     [0x02, 0x10, 0x00, 0x00, 0x00, 0x01, 0x02, 0x42, 0x42, 0x02, 0x31],     #write to slave ID 2 16962 int reg 1
     [0x00, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x18],                        #responce from slave ID 2
